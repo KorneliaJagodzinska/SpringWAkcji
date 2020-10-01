10219793fa436b5a1f3f194b80d9c2a434eac8a5
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-01 12:45:02 +0200

    data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,37 @@
+delete from Taco_Order_Tacos;
+delete from Taco_Ingredients;
+delete from Taco;
+delete from Taco_Order;
+delete from Ingredient;
+
+insert into Ingredient (id, name, type)
+values ('FLTO', 'pszenna', 'WRAP');
+
+insert into Ingredient (id, name, type)
+values ('COTO', 'kukurydziana', 'WRAP');
+
+insert into Ingredient (id, name, type)
+values ('GRBF', 'milona wołowina', 'PROTEIN');
+
+insert into Ingredient (id, name, type)
+values ('CARN', 'kawałki mięsa', 'PROTEIN');
+
+insert into Ingredient (id, name, type)
+values ('TMTO', 'pomidory ', 'VEGGIES');
+
+
+insert into Ingredient (id, name, type)
+values ('LETC', 'sałata ', 'VEGGIES');
+
+insert into Ingredient (id, name, type)
+values ('CHED', 'cheddar ', 'CHEESE');
+
+insert into Ingredient (id, name, type)
+values ('JACK', 'Montery ', 'CHEESE');
+
+insert into Ingredient (id, name, type)
+values ('SLSA', 'pikantny ', 'SAUCE');
+
+insert into Ingredient (id, name, type)
+values ('SRCR', 'śmietana ', 'SAUCE');
+
