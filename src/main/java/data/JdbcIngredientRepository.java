03415cb4ccfb670502463ca4627c86b251df92f3
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-01 11:29:29 +0200

    JdbcIngredientrepostory

--- src/main/java/data/JdbcIngredientRepository.java
+++ src/main/java/data/JdbcIngredientRepository.java
@@ -1,25 +1,37 @@
 package data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import springwakcji.demo.Ingredient;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
     private JdbcTemplate jdbc;
 
+    @Autowired
     public JdbcIngredientRepository(JdbcTemplate jdbc) {
-        this.jdbc=jdbc;
+        this.jdbc = jdbc;
     }
 
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbc.query("select id, name, type from Ingredient", this::mapRowToIngredient);
     }
 
     @Override
     public Ingredient findById(String id) {
-        return null;
+        return jdbc.queryForObject("select id, name, type from Ingredient where id=?", this::mapRowToIngredient, id);
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet rs, int rowNum) throws SQLException {
+        return new Ingredient(
+                rs.getString("id"),
+                rs.getString("name"),
+                Ingredient.Type.valueOf(rs.getString("type")));
     }
 
     @Override
