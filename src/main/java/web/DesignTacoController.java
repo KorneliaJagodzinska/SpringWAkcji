c2bd8e01c732ba8e7bcbd3864ac5360e37669daa
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-01 12:34:21 +0200

    schema.sql

--- src/main/java/web/DesignTacoController.java
+++ src/main/java/web/DesignTacoController.java
@@ -1,6 +1,9 @@
 package web;
 
 import javax.validation.Valid;
+
+import data.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import springwakcji.demo.Ingredient;
 import springwakcji.demo.Ingredient.Type;
 import springwakcji.demo.Taco;
@@ -17,32 +21,41 @@ import springwakcji.demo.Taco;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-@Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
+    private final IngredientRepository ingredientRepo;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
 
     @GetMapping
     public String showDesignForm(Model model){
         List<Ingredient> ingredients= Arrays.asList(
-                new Ingredient("FLTO", "pszenna", Ingredient.Type.WRAP),
-                new Ingredient("COTO", "kukurydziana", Ingredient.Type.WRAP),
-                new Ingredient("GRBF", "milona wołowina", Ingredient.Type.PROTEIN),
-                new Ingredient("CARN", "kawałki mięsa", Ingredient.Type.PROTEIN),
-                new Ingredient("TMTO", "pomidory", Ingredient.Type.VEGGIES),
-                new Ingredient("LETC", "sałata", Ingredient.Type.VEGGIES),
-                new Ingredient("CHED", "cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Montery Jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "pikantny sos pomidorowy", Ingredient.Type.SAUCE),
-                new Ingredient("SRCR", "śmietana", Ingredient.Type.SAUCE)
-        );
-        Ingredient.Type[] types= Ingredient.Type.values();
-        for (Ingredient.Type type: types){
-            model.addAttribute(type.toString().toLowerCase());
-            //filterByType(ingredients, type));
+                ingredientRepo.findAll().forEach(i->ingredients.add(i));
+
+
+//                new Ingredient("FLTO", "pszenna", Ingredient.Type.WRAP),
+//                new Ingredient("COTO", "kukurydziana", Ingredient.Type.WRAP),
+//                new Ingredient("GRBF", "milona wołowina", Ingredient.Type.PROTEIN),
+//                new Ingredient("CARN", "kawałki mięsa", Ingredient.Type.PROTEIN),
+//                new Ingredient("TMTO", "pomidory", Ingredient.Type.VEGGIES),
+//                new Ingredient("LETC", "sałata", Ingredient.Type.VEGGIES),
+//                new Ingredient("CHED", "cheddar", Ingredient.Type.CHEESE),
+//                new Ingredient("JACK", "Montery Jack", Ingredient.Type.CHEESE),
+//                new Ingredient("SLSA", "pikantny sos pomidorowy", Ingredient.Type.SAUCE),
+//                new Ingredient("SRCR", "śmietana", Ingredient.Type.SAUCE)
+//        );
+
+        Type[] types= Ingredient.Type.values();
+        for (Type type: types){
+            model.addAttribute(type.toString().toLowerCase(),
+            filterByType(ingredients, type));
         }
-        model.addAttribute("design", new Taco());
+//        model.addAttribute("design", new Taco());
         return "design";
     }
     @PostMapping
